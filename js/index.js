ec590340b87a7d127c7d043ab817d294f3b67fdc
iGavin21 <2161381@slu.edu.ph>
2018-11-19 10:46:39 +0800

    Added comments

--- js/index.js
+++ js/index.js
@@ -9,12 +9,12 @@ document.addEventListener('DOMContentLoaded', evt => {
       movie: true,
       genres: [],
       keyword: null
-    },
-  
+    }, //declaration of components 
+    
     created: function created() {
       this.$http.get(`${this.baseUrl}/genre/movie/list?api_key=${this.apiKey}&language=en-US`)
         .then(res => this.genres = res.body.genres)
-      this.fetchData();
+      this.fetchData(); //fetch genre of the movie
     },	
 	methods: {
       fetchData: function fetchData() {
@@ -22,13 +22,13 @@ document.addEventListener('DOMContentLoaded', evt => {
         this.$http.get(this.baseUrl + '/discover/movie?api_key=' + this.apiKey + '&sort_by=popularity.desc').then(function (response) {
           console.log(response.body)
           _this.items = response.body;
-          _this.movie = false;
+          _this.movie = false;  //fetch movie according to popularoty
         });
       },
 	  searchMovie (query) {
         this.$http.get(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&language=en-US&page=1&include_adult=false&query=${query}`)
           .then(res => {
-            this.items = res.body
+            this.items = res.body //request for movie search
           })
       }
     }
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', evt => {
 	
 document.getElementById('searchForm').addEventListener('submit', function(evt) {
     evt.preventDefault()
-    homeApp.searchMovie(document.getElementById('searchInput').value)
+    homeApp.searchMovie(document.getElementById('searchInput').value) //gather search input 
   })
 })
 
@@ -51,12 +51,12 @@ document.addEventListener('DOMContentLoaded', evt => {
       movie: true,
       genres: [],
       keyword: null
-    },
+    }, //declaration of components
   
     created: function created() {
       this.$http.get(`${this.baseUrl}/genre/movie/list?api_key=${this.apiKey}&language=en-US`)
         .then(res => this.genres = res.body.genres)
-      this.fetchData();
+      this.fetchData(); //fetch genre of the movie
     },
 	methods: {
       fetchData: function fetchData() {
@@ -64,21 +64,21 @@ document.addEventListener('DOMContentLoaded', evt => {
         this.$http.get(this.baseUrl + '/discover/movie?api_key=' + this.apiKey + '&sort_by=vote_count.desc').then(function (response) {
 		console.log(response.body)
         _this.items = response.body;
-        _this.movie = false;
-        });
+        _this.movie = false; //fetch movie according to rating
+        }); 
       },
 	  searchMovie (query) {
         this.$http.get(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&language=en-US&page=1&include_adult=false&query=${query}`)
           .then(res => {
-            this.items = res.body
-          })
+            this.items = res.body //request for movie search
+          }) 
       }
     }
   });
   document.getElementById('searchForm').addEventListener('submit', function(evt) {
     evt.preventDefault()
-    topmoviesApp.searchMovie(document.getElementById('searchInput').value)
-  })
+    topmoviesApp.searchMovie(document.getElementById('searchInput').value) //gather search input
+  }) 
 })
 
 document.addEventListener('DOMContentLoaded', evt => {
@@ -92,12 +92,12 @@ document.addEventListener('DOMContentLoaded', evt => {
       movie: true,
       genres: [],
       keyword: null
-    },
+    }, //declaration of components
   
     created: function created() {
       this.$http.get(`${this.baseUrl}/genre/movie/list?api_key=${this.apiKey}&language=en-US`)
         .then(res => this.genres = res.body.genres)
-      this.fetchData();
+      this.fetchData(); //fetch genre of the movie
     },
     methods: {
       fetchData: function fetchData() {
@@ -105,20 +105,20 @@ document.addEventListener('DOMContentLoaded', evt => {
         this.$http.get(this.baseUrl + '/discover/movie?api_key=' + this.apiKey + '&sort_by=release_date.desc').then(function (response) {
 		console.log(response.body)
         _this.items = response.body;
-        _this.movie = false;
+        _this.movie = false; //fetch movie according to release date
         });
       },
 	  searchMovie (query) {
         this.$http.get(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&language=en-US&page=1&include_adult=false&query=${query}`)
           .then(res => {
-            this.items = res.body
+            this.items = res.body //request for movie search
           })
       }
     }
   });
   document.getElementById('searchForm').addEventListener('submit', function(evt) {
     evt.preventDefault()
-    upcomingApp.searchMovie(document.getElementById('searchInput').value)
+    upcomingApp.searchMovie(document.getElementById('searchInput').value) //gather search input
   })
 })
 
@@ -130,17 +130,17 @@ new Vue({
     apiKey: '84809cc489047f96f78d2178f63792ef',
     imageUrl: 'https://image.tmdb.orgP/t/p/w342',
     loaded: true
-  },
+  }, //declaration of components
 
   created: function created() {
-    this.fetchData()
+    this.fetchData() //container for gathered data
   },
   methods: {
     fetchData: function fetchData() {
       var _this = this;
       this.$http.get(this.baseUrl + '/discover/movie?api_key=' + this.apiKey).then(function (response) {
         _this.items = response.body;
-        _this.loaded = false;
+        _this.loaded = false; //method for fetching genre of the movie
       })
     }
   }
